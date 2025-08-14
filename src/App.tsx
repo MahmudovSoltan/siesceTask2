89f6a506cb0fb27c0ac15c123fe0a816cb4bf65f
Soltan <mahmudovsoltan477@gmail.com>
2025-08-14 18:12:33 +0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,6 @@ const App: React.FC = () => {
   // Bütün dəyişənlər camelCase formatında
   const userName = "soltan"; // ✅ Düzgün format
   const name = "sss"
-
-  
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
